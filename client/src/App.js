a04abcf51f318ae00f0af057a964342dcb031cf4
ayak002 <aya.khadoum@epitech.eu>
2024-03-23 13:12:39 +0100

    added username

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css'; // Import CSS file if needed
+//import oiseauFlag from './albanie.png';
+//import australieFlag from './australie.png';
+
 
 function CreateGamePage() {
   return (
     <div className="white-page">
       {/* Content for the Create New Game page */}
-      <h1>Create New Game Page</h1>
+      <h1>Create une nouvelle partie</h1>
     </div>
   );
 }
@@ -15,13 +18,14 @@ function JoinGamePage() {
   return (
     <div className="white-page">
       {/* Content for the Join a Game page */}
-      <h1>Join a Game Page</h1>
+      <h1>Joindre une partie</h1>
     </div>
   );
 }
 
 function App() {
   const [menuVisible, setMenuVisible] = useState(true);
+  const [username, setUsername] = useState('');
 
   const handleStartButtonClick = () => {
     // Handle start button click event here
@@ -29,20 +33,65 @@ function App() {
     // You can add additional logic here to start the game
   };
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      // Save the username when ENTER key is pressed
+      const enteredUsername = event.target.value.trim();
+      setUsername(enteredUsername); // Save the username
+      setMenuVisible(true); // Hide the menu
+      event.target.value = ''; // Clear the input field
+    }
+  };
+
+  // Function to import all flag images from a directory
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+// Import all flag images dynamically
+const flagImages = importAll(require.context('./flags/', false, /\.(png)$/));
+
+// Generate React components for each flag image
+const FlagComponents = Object.keys(flagImages).map((imageName, index) => {
+  return (
+    <img key={index} src={flagImages[imageName].default} alt={imageName} />
+  );
+});
+
   return (
     <Router>
       <div className="App">
+        {/* Flags container */}
+        <div className="flag-container">
+          {/* Flags */}
+            {FlagComponents}
+        </div>
+
         <Switch>
           <Route exact path="/">
             {menuVisible ? (
               <div className="menu">
                 <h1>Devine où !</h1>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  onKeyDown={handleKeyDown} // Listen for ENTER key
+                  placeholder="Enter your username"
+                />
                 <Link to="/create-game" className="menu-link">
-                  <button>Create new game</button>
+                  <button>Créer une nouvelle partie</button>
                 </Link>
                 <Link to="/join-game" className="menu-link">
-                  <button>Join a game</button>
+                  <button>Joindre une partie</button>
                 </Link>
+                {username && <p className="username">{username}</p>}
               </div>
             ) : (
               // Render your game components here when the menu is not visible
