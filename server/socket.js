6aaa64cee9a6b55286f2911742955d66224a1950
quentinbraun <quentin.braun@epitech.eu>
2024-03-23 11:55:18 +0100

    [+] working chat thanks to socketio and test html from the get started socketio page

---
+++ server/socket.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/socket.html');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    });
+});
